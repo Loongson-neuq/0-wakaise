939c5db7fb0148f7c7912d65412e7ec38dec2b92
Wakaise <3350283899@qq.com>
2024-10-21 23:09:59 +0800

    完成作业

--- main.cpp
+++ main.cpp
@@ -4,4 +4,47 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    //abc
+    if (a > b && b > c)
+    {
+        result.max = a;
+        result.min = b;
+        result.min = c;
+    }
+    //acb
+    else if (a > c && c > b)
+    {
+        result.max = a;
+        result.min = c;
+        result.min = b;
+    }
+    //bac
+    else if (b > a && a > c)
+    {
+        result.max = b;
+        result.min = a;
+        result.min = c;
+    }
+    //bca
+    else if (b > c && c > a)
+    {
+        result.max = b;
+        result.min = c;
+        result.min = a;
+    }
+    //cab
+    else if (c > a && a > b)
+    {
+        result.max = c;
+        result.min = a;
+        result.min = b;
+    }
+    //cba
+    else if (c > b && b > a)
+    {
+        result.max = c;
+        result.min = b;
+        result.min = a;
+    }
+    return;
 }
